8a1c869148c2efd05dfa2e82130d4c86f8e4cb7f
J.W <jakwings@gmail.com>
2017-07-22 14:18:40 +0800

    Update get_minified_js.sh

--- get_minified_js.sh
+++ get_minified_js.sh
@@ -15,10 +15,10 @@ OUTPUT=dist/tomlify.min.js
 
 curl -s "$API" \
     -d "compilation_level=ADVANCED_OPTIMIZATIONS" \
-    -d "js_externs=window;global;self;module.exports;define.amd;function tomlify(table, replace, space){};tomlify.toKey;tomlify.toValue;" \
+    -d "js_externs=window;global;self;module.exports;define.amd;function tomlify(table, replace, space){};tomlify.toToml;tomlify.toKey;tomlify.toValue;" \
     -d "output_format=text" \
     -d "output_info=compiled_code" \
     -d "language=ECMASCRIPT5_STRICT" \
     --data-urlencode "js_code@$INPUT" \
     -o "$OUTPUT"
-sed -E -i "1s/^('use strict';)?([^{]*)\\{/;\2{'use strict';/" "$OUTPUT"
+#sed -E -i "1s/^('use strict';)?([^{]*)\\{/;\2{'use strict';/" "$OUTPUT"
